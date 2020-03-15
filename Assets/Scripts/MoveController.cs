617ef56d7fcadcfd249bad994bcb077f0b223693
Godzzo <egodzzo@gmail.com>
2020-03-15 06:32:16 +0100

    Fixing Animator SetVelocity - nw working :) not beautiful but ok for Me 🤗

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveController : MonoBehaviour { 
-    public float turnSpeed = 20f;
+    [SerializeField] float _turnSpeed = 20f;
+    [SerializeField] float _speed = 20f;
 
     Animator m_Animator;
     Rigidbody m_Rigidbody;
@@ -23,22 +24,20 @@ public class MoveController : MonoBehaviour {
 
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize();
+        
+        var addToPosition = m_Movement * _speed * Time.deltaTime;
 
-        IsWalking(horizontal, vertical);
+        Debug.Log(string.Format("{0}", addToPosition.ToString("F4")));
 
-        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        SetVelocity(horizontal, vertical);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, _turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
     }
 
-    void IsWalking(float horizontal, float vertical)
+    void SetVelocity(float horizontal, float vertical)
     {
-        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
-        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
-
-        // bool isWalking = hasHorizontalInput || hasVerticalInput;
-
-        // Debug.Log("m_Rigidbody.velocity.magnitude: " + m_Rigidbody.velocity.magnitude);
-        m_Animator.SetFloat("Velocity", m_Rigidbody.velocity.magnitude);
+        m_Animator.SetFloat("Velocity", Mathf.Abs(vertical) > 0? Mathf.Abs(vertical): Mathf.Abs(horizontal));
     }
 
     // https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnAnimatorMove.html
@@ -46,7 +45,8 @@ public class MoveController : MonoBehaviour {
     // This callback will be invoked at each frame after the state machines and the animations have been evaluated, but before OnAnimatorIK.
     void OnAnimatorMove()
     {
-        m_Rigidbody.MovePosition(m_Rigidbody.position + (m_Movement * Time.deltaTime)); //  * m_Animator.deltaPosition.magnitude
+        //  * m_Animator.deltaPosition.magnitude - John Lemon rootMotion???
+        m_Rigidbody.MovePosition(m_Rigidbody.position + (m_Movement * _speed * Time.deltaTime));
         m_Rigidbody.MoveRotation(m_Rotation);
     }
 }
